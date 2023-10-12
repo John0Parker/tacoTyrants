917d6a2724fad872515e9a5143339d0c8a881af2
John Parker <johnnparkerr@gmail.com>
2023-10-12 18:56:54 -0400

    forgot to comment out line 5 in server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 // const cookieParser = require('cookie-parser');
-require('dotenv').config();
+// require('dotenv').config();
 
 app.use(
     cors({
@@ -24,4 +24,4 @@ app.use(
     app.listen(8000, () => console.log('listening on port 8000'));
 
 
-// Cookie Parser lines commented out (not necessary without authentication feature)
+// Cookie Parser & dotenv lines commented out (not necessary without authentication feature)
