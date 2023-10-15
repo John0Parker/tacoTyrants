49ff9fb51d38f93217ad73313cfbe03635c3e087
John Parker <johnnparkerr@gmail.com>
2023-10-15 15:22:54 -0400

    folder maintenance
    Added placeholder files for models, controllers, routes, and components folders. They should now appear correctly on GitHub. Also, once you add a new file to any of those folders, feel free to delete the placeholderFile.

---
+++ client/src/components/placeholderComponent.jsx
@@ -0,0 +1,1 @@
+// This file is just a placeholder for GitHub. Feel free to delete this file once there is another file in this folder.
---
+++ server/controllers/placeholderController.js
@@ -0,0 +1,2 @@
+// This file is just a placeholder for GitHub. Feel free to delete this file once there is another file in this folder.
+
