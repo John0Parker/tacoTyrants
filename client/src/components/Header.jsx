4fbb39f269ce2e0952ad57af2237977ca6646c17
MBaiamonte <120220332+MBaiamonte@users.noreply.github.com>
2023-10-16 21:09:24 -0600

    Header component creation
    Just a header for everyone to use. feel free to change the styling but all functionality should work once everyone puts in their routes to their crud table.

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+const Header =(props)=>{
+    return(
+        <div className="container-fluid">
+            <div className="row">
+                <h1 className="mx-3 my-2"><Link to='/TacoTyrant'  className="text-reset" style={{textDecoration: 'none'}}>Taco Tyrants</Link></h1>
+            </div>
+            <div className="row d-flex justify-content-around">
+                <Link to='' className="btn col-2 btn-outline-primary mb-2 mx-1" style={{textDecoration: 'none'}}>Orders</Link>
+                <Link to='' className="btn col-2 btn-outline-primary mb-2 mx-1" style={{textDecoration: 'none'}}>Menu</Link>
+                <Link to='' className="btn col-2 btn-outline-primary mb-2 mx-1" style={{textDecoration: 'none'}}>Locations</Link>
+                <Link to='/TacoTyrants/AllReviews' className="btn col-2 btn-outline-primary mb-2 mx-1" style={{textDecoration: 'none'}}>Reviews</Link>
+                <Link to='/TacoTyrants/AboutUs' className="btn col-2 btn-outline-primary mb-2 mx-1" style={{textDecoration: 'none'}}>About Us</Link>
+            </div>
+            <hr />
+        </div>
+    )//end return
+}//end homepage
+export default Header;
