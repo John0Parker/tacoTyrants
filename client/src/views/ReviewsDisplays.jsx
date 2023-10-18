e04dc0fd5a7c911017150755cb6f499ce51b176a
MBaiamonte <120220332+MBaiamonte@users.noreply.github.com>
2023-10-17 18:59:09 -0600

    add reviews view display

---
+++ client/src/views/ReviewsDisplays.jsx
@@ -0,0 +1,23 @@
+import { Route,Routes } from "react-router-dom";
+import Header from "../components/Reviews/Header";
+import AllReviews from "../components/Reviews/AllReviews";
+import CreateReview from "../components/Reviews/CreateReview";
+import OneReview from "../components/Reviews/OneReview";
+import UpdateReviewFrom from "../components/Reviews/UpdateReviewForm";
+
+
+const ReviewsDisplays=(props)=>{
+
+    return(
+        <>
+            <Routes>
+                <Route path='/allReviews' element={[<Header/>,<AllReviews/>]}/>
+                <Route path='/review/create' element={[<Header/>,<CreateReview/>]}/>
+                <Route path='/review/:id' element={[<Header/>,<OneReview/>]}/>
+                <Route path='/review/update/:id' element={[<Header/>,<UpdateReviewFrom/>]}/>
+            </Routes>
+        
+        </>
+    )
+}
+export default ReviewsDisplays
