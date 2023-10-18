9a35df49da572e84ddfd8b1e65c2bcd9847eb60d
MBaiamonte <120220332+MBaiamonte@users.noreply.github.com>
2023-10-17 22:12:12 -0600

    Update Header.jsx
    removed the TacoTyrants since it was adding 2x into url, also added link to menu but that might need to be updated. just added to fill the link

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,10 +8,10 @@ const Header =(props)=>{
             </div>
             <div className="row d-flex justify-content-around">
                 <Link to='' className="btn col-2 btn-outline-primary mb-2 mx-1" style={{textDecoration: 'none'}}>Orders</Link>
-                <Link to='' className="btn col-2 btn-outline-primary mb-2 mx-1" style={{textDecoration: 'none'}}>Menu</Link>
+                <Link to='/menu' className="btn col-2 btn-outline-primary mb-2 mx-1" style={{textDecoration: 'none'}}>Menu</Link>
                 <Link to='' className="btn col-2 btn-outline-primary mb-2 mx-1" style={{textDecoration: 'none'}}>Locations</Link>
-                <Link to='/TacoTyrants/AllReviews' className="btn col-2 btn-outline-primary mb-2 mx-1" style={{textDecoration: 'none'}}>Reviews</Link>
-                <Link to='/TacoTyrants/AboutUs' className="btn col-2 btn-outline-primary mb-2 mx-1" style={{textDecoration: 'none'}}>About Us</Link>
+                <Link to='/AllReviews' className="btn col-2 btn-outline-primary mb-2 mx-1" style={{textDecoration: 'none'}}>Reviews</Link>
+                <Link to='/AboutUs' className="btn col-2 btn-outline-primary mb-2 mx-1" style={{textDecoration: 'none'}}>About Us</Link>
             </div>
             <hr />
         </div>
