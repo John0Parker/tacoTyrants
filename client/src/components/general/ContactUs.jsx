4e40c80ebd967bfae1c51928cc89625937b2807d
John Parker <johnnparkerr@gmail.com>
2023-10-19 03:55:25 -0400

    Updated ContactUs component (also added a simple footer to it) and added it to homepage. I just need to style my update and create forms to match the other parts of my section.

--- client/src/components/general/ContactUs.jsx
+++ client/src/components/general/ContactUs.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
 
 const ContactUs = (props) => {
+
+
     return(
-        <div>
-            Filler
-        </div>
+        <>
+            <div class="container">
+                <div class="d-flex justify-content-around">
+                    <div class="card px-3 my-3">
+                        <h4>Questions? Comments? Concerns? We love 'em</h4>
+                        <p class="d-flex justify-content-center mt-1"><strong>email:</strong> tacosaurus@tacotyrants.com</p>
+                        <p class="d-flex justify-content-center"><strong>phone:</strong> (555)219-1213</p>
+                    </div>
+                    <div class="card px-3 my-3">
+                        <h4>Interested in Becoming a Tyrant?</h4>
+                        <span class="d-flex justify-content-center">
+                            <p>Find us on <a href="https://linkedin.com/">
+                                <img height={25} width={45} src="https://myclouddoor.com/wp-content/uploads/2019/11/Linkedin-logo.png" alt="LinkedIn logo" /></a>
+                            </p>
+                        </span>
+                        <span class= "d-flex justify-content-center">
+                            <h5>We're Social </h5>
+                            <img class="ml-2"height={25} width={90} src="https://www.pinclipart.com/picdir/big/323-3234191_png-for-free-download-on-mbtskoudsalg-black-and.png" alt="Icons for Facebook, Twitter, and Instagram," />
+                        </span>
+
+                    </div>
+                </div>
+            </div>
+            <footer class="py-5 my-5">
+                <hr />
+                <p class="text-center text-muted">© 2023 TacoTown LLC. </p>
+            </footer>
+        </>
     )
 }
 
--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import {Route, Routes, Navigate} from 'react-router-dom';
 import AboutUs from '../components/general/AboutUs';
 import Slideshow from '../components/general/Slideshow';
+import ContactUs from '../components/general/ContactUs';
 
 
 const Home = (props) => {
 
     return(
         <Routes>
-            <Route path='/Homepage' element={[<Slideshow/>]} />
+            <Route path='/Homepage' element={[<Slideshow/>, <ContactUs/>]} />
             {/* ^^^^^^^  add contact us component to route elements above  ^^^^^^^ */}
             <Route path='/AboutUs' element={<AboutUs/>} />
         </Routes>
