0e9de664e6a709f32517f5d7565fd217b46045b5
MBaiamonte <120220332+MBaiamonte@users.noreply.github.com>
2023-10-18 19:28:47 -0600

    Update Header.jsx
    fixed routing to homepage

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header =(props)=>{
     return(
         <div className="container-fluid">
             <div className="row">
-                <h1 className="mx-3 my-2"><Link to='/TacoTyrant' style={{textDecoration: 'none'}}>Taco Tyrants</Link></h1>
+                <h1 className="mx-3 my-2"><Link to='/Homepage' style={{textDecoration: 'none'}}>Taco Tyrants</Link></h1>
             </div>
             <div className="row d-flex justify-content-around">
                 <Link to='/orders' className="btn col-2 btn-outline-primary mb-2 mx-1" style={{textDecoration: 'none'}}>Orders</Link>
