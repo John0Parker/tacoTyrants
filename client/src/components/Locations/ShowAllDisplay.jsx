857408feef7f878b1c9d97dfd1e50927b4529e00
Adam Files <a.files21@gmail.com>
2023-10-18 20:46:13 -0700

    additional styling, format phone number and date created

--- client/src/components/Locations/ShowAllDisplay.jsx
+++ client/src/components/Locations/ShowAllDisplay.jsx
@@ -16,18 +16,32 @@ const ShowAllDisplay = ({allTacoLocations, setAllTacoLocations}) => {
         <div className='container'>
             {
                 allTacoLocations.map( (store, index) => {
+                    const phoneNumber = store.phoneNumber.toString();
+                            let phoneArr = [];
+                            const phoneFormat = phoneNum => {
+                                for(let i = 0; i < phoneNum.length; i++) {
+                                    if(i == 2 || i == 5) {
+                                        phoneArr.push(phoneNum[i]);
+                                        phoneArr.push('-');
+                                    } else {
+                                        phoneArr.push(phoneNum[i]);
+                                    }
+                                }
+                                return phoneArr;
+                            }
+                            phoneFormat(phoneNumber);
                     return(
-                        <div className='card mb-3'>
+                        <div className='card mb-3 locationCard'>
                             <div className='row g-0 p-5'>
                                 <div className='col-md-4'>
                                     <img src={store.locationImage} className='img-fluid rounded-start' style={{maxWidth: "400px"}} alt="a photo of this Taco Tyrant Shop" />
                                 </div>
                                 <div className='col-md-8'>
                                     <div className='card-body mx-5'>
-                                        <h3 class="card-title">Address: {store.address}</h3>
-                                        <p className="card-text">Phone Number: {store.phoneNumber}</p>
+                                        <h3 className="card-title">Address: {store.address}</h3>
+                                        <p className="card-text">Phone Number: {phoneArr}</p>
                                         <p className="card-text">Hours: {store.hours}</p>
-                                        <p className="card-text">Leave a review: <Link className='btn btn-outline-warning text-warning-emphasis' to={'/review/create'}>Review Form</Link></p>
+                                        <p className="card-text">Leave a review: <Link className='btn btn-outline-primary text-primary-emphasis' to={'/review/create'}>Review Form</Link></p>
                                         <Link className='btn btn-outline-success text-success-emphasis' to={'/locations/view/' + store._id}>View Taco Shop</Link>
                                         <Link className='btn btn-outline-success text-success-emphasis mx-3' to={'/locations/update/' + store._id}>Edit</Link>
                                     </div>
